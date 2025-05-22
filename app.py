3163847e9ad7855706eb70084b0440520b28bd98
Roger Oliveira <rogerfutsa@gmail.com>
2025-05-22 01:31:03 -0300

    att 2

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from flask import Flask, request, jsonify
 from services.diagnosticos.diagnostico_bovinos_service import DiagnosticoBovinosService
 from services.diagnosticos.diagnostico_cachorro_service import DiagnosticoCachorroService
 from services.diagnosticos.diagnostico_calopsita_service import DiagnosticoCalopsitaService
+from services.diagnosticos.diagnostico_caprinos_service import DiagnosticoCaprinoService
+from services.diagnosticos.diagnostico_equinos_service import DiagnosticoEquinoService
 from services.diagnosticos.diagnostico_ovinos_service import DiagnosticoOvinosService
+from services.diagnosticos.diagnostico_suinos_service import DiagnosticoSuinoService
+from services.diagnosticos.diagnostico_tartaruga_service import DiagnosticoTartarugaService
 
 app = Flask(__name__)
 serviceCachorro = DiagnosticoCachorroService()
 serviceCalopsita = DiagnosticoCalopsitaService()
 serviceOvinos = DiagnosticoOvinosService()
 serviceBovinos = DiagnosticoBovinosService()
+serviceCaprino = DiagnosticoCaprinoService()
+serviceEquino = DiagnosticoEquinoService()
+serviceSuino = DiagnosticoSuinoService()
+serviceTartaruga = DiagnosticoTartarugaService()
 
 @app.route('/diagnostico/cao', methods=['POST'])
 def prever_diagnostico_cao():
@@ -61,6 +69,58 @@ def prever_diagnostico_bovinos():
         'prever_diagnostico': previsao,
         'nome_da_doenca': nome_doenca
     })            
+
+@app.route('/diagnostico/caprinos', methods=['POST'])
+def prever_diagnostico_caprinos():
+    data = request.json
+    previsao, nome_doenca = serviceCaprino.prever(data)
+
+    print(f"Previsão numérica: {previsao}")
+    print(f"Doença identificada: {nome_doenca}")
+
+    return jsonify({
+        'prever_diagnostico': previsao,
+        'nome_da_doenca': nome_doenca
+    })
+
+@app.route('/diagnostico/equinos', methods=['POST'])
+def prever_diagnostico_equinos():
+    data = request.json
+    previsao, nome_doenca = serviceEquino.prever(data)
+
+    print(f"Previsão numérica: {previsao}")
+    print(f"Doença identificada: {nome_doenca}")
+
+    return jsonify({
+        'prever_diagnostico': previsao,
+        'nome_da_doenca': nome_doenca
+    })
+
+@app.route('/diagnostico/suinos', methods=['POST'])
+def prever_diagnostico_suinos():
+    data = request.json
+    previsao, nome_doenca = serviceSuino.prever(data)
+
+    print(f"Previsão numérica: {previsao}")
+    print(f"Doença identificada: {nome_doenca}")
+
+    return jsonify({
+        'prever_diagnostico': previsao,
+        'nome_da_doenca': nome_doenca
+    })
+
+@app.route('/diagnostico/tartaruga', methods=['POST'])
+def prever_diagnostico_tartaruga():
+    data = request.json
+    previsao, nome_doenca = serviceTartaruga.prever(data)
+
+    print(f"Previsão numérica: {previsao}")
+    print(f"Doença identificada: {nome_doenca}")
+
+    return jsonify({
+        'prever_diagnostico': previsao,
+        'nome_da_doenca': nome_doenca
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
